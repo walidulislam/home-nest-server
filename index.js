100b9350176c907b4fb9656d711d6ca8645e43de
Walidul Islam <walidulislam2005@gmail.com>
2025-12-07 19:44:31 +0600

    home nest server connected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion} = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 //Middleware....
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@applicationuser1.8vmcaeg.mongodb.net/?appName=applicationUser1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
