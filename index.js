f769563be7f8427bc559c30135d12f2aa4c255b9
Walidul Islam <walidulislam2005@gmail.com>
2025-12-08 15:20:42 +0600

    get ratings by user email in ratings collection

--- index.js
+++ index.js
@@ -103,6 +103,15 @@ async function run() {
       res.send(result);
     });
 
+    //Get ratings by user email
+    app.get("/my-ratings", async (req, res) => {
+      const { email } = req.query;
+      const result = await ratingsCollection
+        .find({ reviewerEmail: email })
+        .toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
